5c7213fd22e47555ed554889bc4dde64a871386a
Richard Lee <rlee@tokyo3.com>
2023-03-03 08:02:53 -0800

    refactor out prompt file generation.  simplifies going from preamble/context to new context

--- generate_seed_assets.sh
+++ generate_seed_assets.sh
@@ -270,13 +270,14 @@ refine_context_with_preamble() {
         fatal "context file for $2 does not exist: $1"
     fi
 
-    if [ -f "3" ]; then
+    if [ -f "$3" ]; then
         info "context file for $2 already exists: $1"
-    fi$
+    fi
 
     _X_NEW_CONTEXT_PROMPT_FILE="$SCRATCH_DIR/${2}_prompt.txt"
     create_prompt_file "$4" "$_X_NEW_CONTEXT_PROMPT_FILE" "$1"
-    generate_completion_from_prompt "$_X_NEW_CONTEXT_FILE" "$2" "$3"
+
+    generate_completion_from_prompt "$_X_NEW_CONTEXT_PROMPT_FILE" "$2" "$3"
 }
 
 if [ "X$1" == "X" ]; then
@@ -298,40 +299,29 @@ if [ ! -d "$SCATCH_DIR" ]; then
     fi
 fi
 
-ORIGINAL_PROMPT_FILE="$SCRATCH_DIR/original_prompt.txt"
-if [ ! -f "$ORIGINAL_PROMPT_FILE" ]; then
+USER_PROVIDED_PROMPT_FILE="$SCRATCH_DIR/original_prompt.txt"
+if [ ! -f "$USER_PROVIDED_PROMPT_FILE" ]; then
     # record and store the original user prompt
-    echo "$2" > "$ORIGINAL_PROMPT_FILE"
+    echo "$2" > "$USER_PROVIDED_PROMPT_FILE"
     info "Using initial prompt: $2"
-    info "Creating Original prompt file: $ORIGINAL_PROMPT_FILE"
 else
-    info "Original prompt File already exists: $ORIGINAL_PROMPT_FILE"
+    info "User provided prompt file already exists: $USER_PROVIDED_PROMPT_FILE"
 fi
 
-# create the initial complete prompt file with prompt instruction
-INITIAL_PROMPT_PREAMBLE="Given the following prompt, if not already specified choose a setting from an arbitrary selection of top 25 countries by GDP other than the United States. If not already specified, choose an arbitrary time period for the story.  valid time periods are prehistoric, feudal, renaissance, post industrial, modern, or futuristic. Summarize a story using the Pixar method.  the story should take place in a few key concrete locations and involve both main characters and supporting roles. Half of the roles should be from a different country. If a role represents a group of people create a two or three representative individuals. Give each character a full name an age and description. Create a detailed description of this context including all locations, characters and a synopsis of how all of the story elements are related. "
 
-INITIAL_PROMPT_FILE="$SCRATCH_DIR/initial_prompt.txt"
-create_prompt_file "$INITIAL_PROMPT_PREAMBLE" "$INITIAL_PROMPT_FILE" "$ORIGINAL_PROMPT_FILE"
+STORY_PROMPT_PREAMBLE="Given the following prompt, if not already specified choose a setting from an arbitrary selection of top 25 countries by GDP other than the United States. If not already specified, choose an arbitrary time period for the story.  valid time periods are prehistoric, feudal, renaissance, post industrial, modern, or futuristic. Summarize a story using the Pixar method.  the story should take place in a few key concrete locations and involve both main characters and supporting roles. Half of the roles should be from a different country. If a role represents a group of people create a two or three representative individuals. Give each character a full name an age and description. Create a detailed description of this context including all locations, characters and a synopsis of how all of the story elements are related. "
+STORY_CONTEXT_FILE="$SCRATCH_DIR/story_context.txt"
 
-MAIN_CONTEXT_FILE="$SCRATCH_DIR/main_context.txt"
-info "Creating main context file: $MAIN_CONTEXT_FILE"
-generate_completion_from_prompt "$INITIAL_PROMPT_FILE" main_context "$MAIN_CONTEXT_FILE"
-
-# $1 original_context_file
-# $2 new_context_name
-# $3 new_context_file
-# $4 preamble
-# refine_context_with_preamble "$ORIGINAL_PROMPT_FILE" main_context "$MAIN_CONTEXT_FILE" "$INITIAL_PROMPT_PREAMBLE"
+info "Creating story context"
+refine_context_with_preamble "$USER_PROVIDED_PROMPT_FILE" story_context "$STORY_CONTEXT_FILE" "$STORY_PROMPT_PREAMBLE"
 
 CHARACTER_LIST_PREAMBLE="Given the following context if there are roles representing a group of people, create two or three representative characters from that group. Give each character a full name, age, sex, hair color, eye color, birth date, a zodiac sign consistent with their birth date, blood type, birth place,  and description. create a list of all characters in the story in csv format where the first column is a unix filename based on the name of the character with spaces converted into underscores and is prefixed with character and has a .json extension.  The second column is the full name of the character.  The third column is a brief description of the character's role in the story."
-CHARACTER_LIST_PROMPT_FILE="$SCRATCH_DIR/character_list_prompt.txt"
-create_prompt_file "$CHARACTER_LIST_PREAMBLE" "$CHARACTER_LIST_PROMPT_FILE" "$MAIN_CONTEXT_FILE"
-
 CHARACTER_LIST_CSV="$SCRATCH_DIR/character_list.csv"
-generate_completion_from_prompt "$CHARACTER_LIST_PROMPT_FILE" character_list "$CHARACTER_LIST_CSV" .json
 
-CHARACTER_FILES=`cat "$CHARACTER_LIST_CSV" | cut -f 1 -d,`
+info "Creating character list"
+refine_context_with_preamble "$STORY_CONTEXT_FILE" character_list "$CHARACTER_LIST_CSV" "$CHARACTER_LIST_PREAMBLE"
+
+CHARACTER_FILES=`cat "$CHARACTER_LIST_CSV" | grep '.json' | cut -f 1 -d,`
 for CHARACTER_FILE in $CHARACTER_FILES; do
     CHARACTER_FILE_PREFIX=`basename "$CHARACTER_FILE" .json`
 
@@ -341,31 +331,27 @@ for CHARACTER_FILE in $CHARACTER_FILES; do
     fi
     CHARACTER_NAME=`echo -n "$CHARACTER_ROW" | cut -f 2 -d,`
 
-    info "Generating character: $CHARACTER_NAME"
+    info "Creating character: $CHARACTER_NAME"
 
+    CHARACTER_CONTEXT_NAME="${CHARACTER_FILE_PREFIX}_context"
     CHARACTER_CONTEXT_PREAMBLE="given the following context describe the character $CHARACTER_NAME, their motivations their life before the story and their role in the story details should also include age, sex, hair color, eye color, birth date, a zodiac sign consistent with their birth date, blood type and birth place. If their place of birth is not in the setting of the story, describe why they left their birth place"
-    CHARACTER_CONTEXT_PROMPT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_context_prompt.txt"
-    create_prompt_file "$CHARACTER_CONTEXT_PREAMBLE" "$CHARACTER_CONTEXT_PROMPT_FILE" "$MAIN_CONTEXT_FILE"
-    CHARACTER_CONTEXT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_context.txt"
-    generate_completion_from_prompt "$CHARACTER_CONTEXT_PROMPT_FILE" "${CHARACTER_FILE_PREFIX}_context" "$CHARACTER_CONTEXT_FILE"
+    CHARACTER_CONTEXT_FILE="$SCRATCH_DIR/${CHARACTER_CONTEXT_NAME}.txt"
+    refine_context_with_preamble "$STORY_CONTEXT_FILE" "$CHARACTER_CONTEXT_NAME" "$CHARACTER_CONTEXT_FILE" "$CHARACTER_CONTEXT_PREAMBLE"
 
+    CHARACTER_ATTRIBUTES_NAME="${CHARACTER_FILE_PREFIX}_attributes"
     CHARACTER_ATTRIBUTES_PREAMBLE="given the following character information create a list of character attributes in csv format list where the first column is the name of an attribute and the second column is the value based on the character description. The rows are name, age, sex, hair color, eye color, birthdate, zodiac sign, blood type, and birth place."
-    CHARACTER_ATTRIBUTES_PROMPT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_attributes_prompt.txt"
-    create_prompt_file "$CHARACTER_ATTRIBUTES_PREAMBLE" "$CHARACTER_ATTRIBUTES_PROMPT_FILE" "$CHARACTER_CONTEXT_FILE"
-    CHARACTER_ATTRIBUTES_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_attributes.txt"
-    generate_completion_from_prompt "$CHARACTER_ATTRIBUTES_PROMPT_FILE" "${CHARACTER_FILE_PREFIX}_attributes" "$CHARACTER_ATTRIBUTES_FILE"
+    CHARACTER_ATTRIBUTES_CSV="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_attributes.csv"
+    refine_context_with_preamble "$CHARACTER_CONTEXT_FILE" "$CHARACTER_ATTRIBUTES_NAME" "$CHARACTER_ATTRIBUTES_CSV" "$CHARACTER_ATTRIBUTES_PREAMBLE"
 
+    CHARACTER_DETAIL_NAME="${CHARACTER_FILE_PREFIX}_detail"
     CHARACTER_DETAIL_PREAMBLE="given the following context describe the character $CHARACTER_NAME create a detailed physical description of the character including physical features that make them stand out as well as any possessions they will always have on themselves.  The description should also include clothing preferences. "
-    CHARACTER_DETAIL_PROMPT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_detail_prompt.txt"
-    create_prompt_file "$CHARACTER_DETAIL_PREAMBLE" "$CHARACTER_DETAIL_PROMPT_FILE" "$CHARACTER_CONTEXT_FILE"
     CHARACTER_DETAIL_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_detail.txt"
-    generate_completion_from_prompt "$CHARACTER_DETAIL_PROMPT_FILE" "${CHARACTER_FILE_PREFIX}_detail" "$CHARACTER_DETAIL_FILE"
+    refine_context_with_preamble "$CHARACTER_CONTEXT_FILE" "$CHARACTER_DETAIL_NAME" "$CHARACTER_DETAIL_FILE" "$CHARACTER_DETAIL_PREAMBLE"
 
+    CHARACTER_PORTRAIT_CONTEXT_NAME="${CHARACTER_FILE_PREFIX}_portrait_context"
     CHARACTER_PORTRAIT_CONTEXT_PREAMBLE="write a DALL-E prompt for a portrait of the following person that is no longer than 900 characters long. "
-    CHARACTER_PORTRAIT_CONTEXT_PROMPT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_portrait_context_prompt.txt"
-    create_prompt_file "$CHARACTER_PORTRAIT_CONTEXT_PREAMBLE" "$CHARACTER_PORTRAIT_CONTEXT_PROMPT_FILE" "$CHARACTER_DETAIL_FILE"
     CHARACTER_PORTRAIT_CONTEXT_FILE="$SCRATCH_DIR/${CHARACTER_FILE_PREFIX}_portrait_context.txt"
-    generate_completion_from_prompt "$CHARACTER_PORTRAIT_CONTEXT_PROMPT_FILE" "${CHARACTER_FILE_PREFIX}_portrait_context" "$CHARACTER_PORTRAIT_CONTEXT_FILE"
+    refine_context_with_preamble "$CHARACTER_DETAIL_FILE" "$CHARACTER_PORTRAIT_CONTEXT_NAME" "$CHARACTER_PORTRAIT_CONTEXT_FILE" "$CHARACTER_PORTRAIT_CONTEXT_PREAMBLE"
 
     CHARACTER_PORTRAIT_PREAMBLE="take a photographic portrait with a 35mm lens of the following character"
     CHARACTER_PORTRAIT_SUFFIX="35mm, fujifilm, dramatic lighting, cinematic, 4k"
